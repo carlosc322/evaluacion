02134a038401867373e1aaefa5e14e89854a2b8f
carlosc322 <carlchoque52@gmail.com>
2025-09-08 21:57:12 -0300

    Tercer Commit
    Creación de la app página_deportiva y su configuración en el

--- PáginaDeportiva/PáginaDeportiva/settings.py
+++ PáginaDeportiva/PáginaDeportiva/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuario',
+    'página_deportiva',
+
 ]
 
 MIDDLEWARE = [
---
+++ PáginaDeportiva/página_deportiva/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PáginaDeportivaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'página_deportiva'
